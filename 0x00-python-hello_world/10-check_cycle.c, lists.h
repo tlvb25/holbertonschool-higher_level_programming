4aa087bddb803f3fc021ac13722955de56629798
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-29 23:31:11 -0700

    Floyd Cycle Detection algorithm

---
+++ 0x00-python-hello_world/10-check_cycle.c, lists.h
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - function that detects if list has cycle (loop)
+ * @list: linked list passed-in to check
+ *
+ * Return: 0 if no cycle 1 if there is
+ */
+int check_cycle(listint_t *list)
+{
+        listint_t *slow=list;
+        listint_t *fast=list;
+        
+        while(slow && fast && fast->next!=NULL) 
+        {
+            
+            slow = slow->next;               //Slow pointer moves by 1 step 
+            fast = fast->next->next;        //Fast pointer moves by two steps
+            
+            if(slow==fast) //If they meet then there is a loop
+                return true;
+        }
+        return false; //No loop
+}
