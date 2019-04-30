64a14e816e05c6ef617ea21de1e2a99aa17fc4c3
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-04-30 00:05:05 -0700

    code style correction

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,8 +2,3 @@
 from sys import stderr
 stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 exit(1)
-
-'''
-used help from website 
-"https://www.programcreek.com/python/example/56344/sys.stderr.write"
-'''
