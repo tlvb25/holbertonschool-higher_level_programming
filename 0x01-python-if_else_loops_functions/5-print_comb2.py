7dc7fb4ec64d9b085619a3aa7e06119fcf8e1a35
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-01 14:21:25 -0700

    code for task #5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-def fizzbuzz():
+for x in range(0,99):
+        print("{:02d}".format(x), end=", ")
+print("99")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,1 @@
 #!/usr/bin/python3
-listint_t *insert_node(listint_t **head, int number);
