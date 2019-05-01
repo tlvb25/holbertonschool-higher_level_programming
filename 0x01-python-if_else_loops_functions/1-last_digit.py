022cc8d0f636d7a0a5c95a96d59efb474313c308
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-01 00:50:53 -0700

    prog assgn rdm num to var num each run

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = number % 10
+last_digit = abs(number) % 10
+
 
 print("Last digit of {} is {} ".format(number, last_digit), end="")
 
