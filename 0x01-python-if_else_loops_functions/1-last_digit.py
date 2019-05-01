7b939c6372edbab6626a32eb3a51a0799ae3c734
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-01 00:47:01 -0700

    prog assigns random signed num to var num each run

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 
-last = (number % 10)
+last_digit = number % 10
 
-if last > 5:
-        print("Last digit of ", number, " is ", last, " and is greater than 5")
-elif last == 0:
-        print("Last digit of ", number, " is ", last, " and is 0")
-else:
-        print("Last digit of ", number, " is ", last, " and is less than 6 and not 0")
+print("Last digit of {} is {} ".format(number, last_digit), end="")
+
+if last_digit > 5:
+        print("and is greater than 5")
+elif last_digit == 0:
+        print("and is 0")
+elif last_digit < 6 and last_digit != 0:
+        print("and is less than 6 and not 0")
