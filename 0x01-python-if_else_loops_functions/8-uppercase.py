d5b1db65824052cf236c48c3783426bb2e5a08c9
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-01 21:42:36 -0700

    func prints a str in uppercase follw'd by new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def uppercase(str):
+        upper_str = ''
+        for c in str:
+                if ord(c) >= 65:
+                        upper_str += chr(ord(c) - 32)
+                else:
+                        upper_str = c
+        print("{:c}".format(upper_str), end="")
+        print('')
