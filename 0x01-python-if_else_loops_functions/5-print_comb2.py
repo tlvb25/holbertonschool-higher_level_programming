2ac0006de38b788367d90b14c855a7d268097eb7
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-01 20:49:19 -0700

    function that checks for lowercase character

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-        print("{:02d}".format(x), end=", ")
-print("99")
+for i in range(00, 98):
+        print("{:02d}".format(i), end=", ")
+print('99')
