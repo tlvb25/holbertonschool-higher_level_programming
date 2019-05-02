5db83d227d619316036d3ae4966f697e3e81d743
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-02 00:02:59 -0700

    code correction

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alpha in range(ord('z'), ord('`'), -1):
-        if alpha % 2 == 1:
-                print("{}".format(chr(alpha - 32)), end='')
-        else:
-                print("{}".format(chr(alpha)), end='')
+    if alpha % 2 == 1:
+        print("{}".format(chr(alpha - 32)), end='')
+    else:
+        print("{}".format(chr(alpha)), end='')
