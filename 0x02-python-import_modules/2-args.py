9256de013491b6e90adb6b1f55072ed39ad7c582
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-02 19:01:53 -0700

    code correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
                 print("{:d} argument:".format(args - 1))
                 print("{:d}: {:s}".format(args - 1, argv[1]))
         else:
-                print("{:d} arguments:".format(args -1))
+                print("{:d} arguments:".format(args - 1))
                 for i in range(1, args):
                         print("{:d}: {:s}".format(i, argv[i]))
