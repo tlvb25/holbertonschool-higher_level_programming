e7dc15bbd3d0ae108683dd82d3666564b38e0c63
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-02 17:26:39 -0700

    testing code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+        import sys
+
+        args = len(sys.argv)
+
+        if args == 1:
+                print("0 arguments.")
+        elif args > 1:
+                print("{:d} arguments:".format(args))
+                for i in range(args):
+                        print("{:d} {:s}".format(i, args[i]))
