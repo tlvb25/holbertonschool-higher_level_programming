9dd22d5202b964fbc3e6f6fc388b86a3d8c5004d
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-02 23:09:41 -0700

    code correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-    from calculator_1 import add, sub, add, mul
+    from calculator_1 import add, sub, div, mul
 
     args = len(argv)
 
