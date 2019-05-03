2d5f7aca325d955599ed55964e9d579044ffc445
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-02 23:05:19 -0700

    code correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,20 @@
 if __name__ == '__main__':
     from sys import argv
     from calculator_1 import add, sub, add, mul
+
     args = len(argv)
-    a = int(argv[1])
-    b = int(argv[3])
-    c = argv[2]
+
+    operan = {add: "+", sub: "-", mul: "*", div: "/"}
+
     if args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if c == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif c == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif c == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif c == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    elif argv[2] in operan:
+        a = int(argv[1])
+        b = int(argv[3])
+        evaluation = operan[argv[2]]
+        result = evaluation(a, b)
+        print("{} {} {} = {}".format(a, argv[2], b, result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-        
