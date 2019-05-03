509bcc8390d1e06f5ed5c05b9ccb0cf029f74d15
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-02 18:08:02 -0700

    testing code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+if __name__ == '__main__':
+        from sys import argv
+        from add_0 import add
+
+        args = len(argv)
+        sum = 0
+
+        if args == 1:
+                print("{:d}".format(sum))
+        else:
+                print("{:d} arguments:".format(args -1))
+                for i in range(1, args):
+                        sum += int(argv[i])
+                        print("{:d}".format(sum))
