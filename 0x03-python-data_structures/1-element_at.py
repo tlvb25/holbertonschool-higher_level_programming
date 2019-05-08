7e7f16d8a3affd489c56e298b851ca247fcb93bb
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-07 22:09:48 -0700

    code corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
         if idx < 0:
-                return (None)
+                return None
         elif idx > len(my_list):
-                return (None)
+                return None
         else:
-                res_list = [my_list[i] for i in idx]
-                return(res_list)
+                return [my_list[idx]
