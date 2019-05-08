bc5d9071099160a2f30daaa905ff3ebf09a55ad6
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-07 23:56:56 -0700

    style change

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
         new_string = ""
 
         for i in my_string:
-                if i != 'c' or i != 'C':
+                if (i != 'c' or i != 'C'):
                         new_string += i
         return new_string
