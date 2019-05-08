1d21460a8b036d4c730dcaedef1bb06af5ecc4a5
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-07 23:25:20 -0700

    code correction

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,6 @@ def new_in_list(my_list, idx, element):
     if idx > len(my_list) - 1:
         return my_list
     else:
-        if my_list:
-                new_list = my_list
-                new_list[idx] = element
-                return new_list
+        new_list = [my_list]
+        new_list[idx] = element
+        return new_list
