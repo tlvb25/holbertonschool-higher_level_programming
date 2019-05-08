9d78db86e251b197ae6a92dab121b23945476f08
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-07 23:32:18 -0700

    code change

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
     if idx > len(my_list) - 1:
         return my_list
     else:
-        new_list = my_list
+        new_list = [i for in my_list]
         new_list[idx] = element
         new_list
         return new_list
