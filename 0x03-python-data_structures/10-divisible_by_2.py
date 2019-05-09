0a484e9af594c8463c7b81cfaac43c15b10a3092
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-08 23:29:21 -0700

    code changes

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
 
         for num in my_list:
                 if num % 2 == 0:
-                        return new_list.append(True)
+                        new_list.append(True)
                 else:
-                        return new_list.append(False)
+                        new_list.append(False)
         return new_list
