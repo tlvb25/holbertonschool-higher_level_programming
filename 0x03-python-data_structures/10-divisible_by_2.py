3dd2ff623593f767dab6e28f0a94f019a80b111f
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-08 23:27:20 -0700

    code changes for task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+        new_list = []
+
         for num in my_list:
-                if num / 2 == 0:
-                        return True
+                if num % 2 == 0:
+                        return new_list.append(True)
                 else:
-                        return False
+                        return new_list.append(False)
+        return new_list
