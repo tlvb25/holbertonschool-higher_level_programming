2697206c77269eff7b38a0e71bb682cdf698db5e
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-08 22:17:02 -0700

    code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-        tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
-        return tuple
+        return (tuple(map(sum, zip(tuple_a, tuple_b))))
