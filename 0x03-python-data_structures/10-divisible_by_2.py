9437940f694b12639e75a7e53a77ed17a21d96cd
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-08 23:19:15 -0700

    code for task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+        for num in my_list:
+                if num / 2 == 0:
+                        return True
+                else:
+                        return False
+                        
