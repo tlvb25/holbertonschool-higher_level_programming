59f46d1f13e6d56cbf997fc51eea765e46e63660
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-10 09:47:56 -0700

    code for task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+        max_val = None
+        max_key = None
+
+        for k, v in dict.items():
+                if max_val is None or max_val < v:
+                        max_val = v
+                        max_key = k
+        return max_key
