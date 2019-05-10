ad737ec323f6f33d23dfbaa26416ba68bdc136b6
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-10 00:04:38 -0700

    code for task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+        if my_list == "":
+                return 0
+        sum1 = 0
+        for tuples in my_list:
+                sum1 += (tuples[0] * tuples[1])
+        sum2 = 0
+        for tuples in my_list:
+                sum2 += tuples[1]
+        return sum1 / sum2
