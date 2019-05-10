f9e288754a66a2f4cb4fcfb58986b57215a5439e
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-09 23:01:37 -0700

    code for task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+        new_list = []
+
+        for _, v in enumerate(my_list):
+                # using enumerate function and list comprehension
+                new_list.append(replace if v == search else v)
+
+        return (new_list)
