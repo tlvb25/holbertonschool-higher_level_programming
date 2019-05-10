3a4f5323af1cb0d9ab5bd1188b516f7c71fc897e
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-09 23:54:36 -0700

    code for task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+        new_dictionary = a_dictionary.copy()
+
+        for x in new_dictionary:
+                new_dictionary[x] *= 2
+
+        return new_dictionary
