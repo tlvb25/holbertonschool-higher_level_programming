1ddf81f572ab78ec6485d21cbe1cda02788a27d1
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-20 23:55:21 -0700

    code corrections

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
+def safe_print_quotientision(a, b):
     try:
-        quotient = a / b    
-    except ZeroDivisionError:
+        quotient = a / b
+    except ZeroquotientisionError:
         quotient = None
     finally:
-        print('Inside result: {}'.format(quotient))
+        print("Inside result: {}".format(quotient))
     return quotient
+    
