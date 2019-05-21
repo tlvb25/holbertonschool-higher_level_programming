8e7416c3c1d01d4394e4fdd191b01a206dc63fcb
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-20 23:44:41 -0700

    code for task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-        nb_print = [x for x in my_list]
-        print(nb_print)
-        return x
+    count = 0
+    for i in range(x):
+        try:
+            print('{:d}'.format(my_list[i]), end="")
+            count += 1
+        except:
+            pass
+    print()
+    return count
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    try:
+        quotient = a / b    
+    except (UnboundLocalError, ZeroDivisionError):
+        quotient = None
+    finally:
+        print('Inside result: {}'.format(quotient))
+        return quotient
