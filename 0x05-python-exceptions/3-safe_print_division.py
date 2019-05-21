2785fb6a96b5ad625e81ea1b6e55e814f8080f2d
Tywan Brooks <tbrooks@MacBook-Air.local>
2019-05-21 00:05:13 -0700

    removed whitespace

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        quotient = a / b    
+        quotient = a / b
     except ZeroDivisionError:
         quotient = None
     finally:
