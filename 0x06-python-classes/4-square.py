a675cc0546426b1969b27225c78df7790a5054e1
T <test@mail.com>
2019-05-21 21:50:42 -0700

    code for task 4 and 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,11 +4,11 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-
+    @property
     def size(self):
         return self.__size
 
-
+    @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError('size must be an integer')
