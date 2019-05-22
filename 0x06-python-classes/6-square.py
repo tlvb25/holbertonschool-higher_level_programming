ca12b42cb0638943b860026b212ff5e3df1f5011
T <test@mail.com>
2019-05-21 22:26:52 -0700

    code corrections added decorators

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,6 +17,7 @@ class Square:
     def size(self):
         return self.__size
 
+    @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) is not 2:
             raise TypeError('size must be an integer')
@@ -25,6 +26,10 @@ class Square:
         elif type(value[0]) < 0 or type(value[1]) < 0:
             raise TypeError('size must be an integer')
         self.__position = value
+
+    @property
+    def position(self):
+        return self.__position
 
     def area(self):
         return self.__size ** 2
