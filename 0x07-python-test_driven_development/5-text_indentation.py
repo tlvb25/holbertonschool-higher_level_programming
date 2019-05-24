405705ede9b085bf1f9273f25bdafa6c1f68f9de
T <test@mail.com>
2019-05-24 02:59:42 -0700

    code correcton for task

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,7 +4,7 @@ def text_indentation(text):
         raise TypeError('text must be a string')
 
     for char in text:
-        print(char, end="")
+        print(char.strip(), end="")
         if char is '.' or char is '?' or char is ':':
             print()
             print()
