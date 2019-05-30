38d6c5ec75959d87f20062e98806dba00e3c4b9a
T <test@mail.com>
2019-05-30 15:20:20 -0700

    code corrections

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -47,7 +47,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
         custom_symbol = str(self.print_symbol)
-        string = (custom_symbol * self.__width + '\n') * (self.__height)
+        string = (custom_symbol * self.__width + '\n') * (self.__height -1) + (
+            custom_symbol * self.__width)
         return string
 
     def __repr__(self):
