40e265089505cbb82cee20e73ad7b6d3198acf9a
T <test@mail.com>
2019-05-30 15:17:23 -0700

    code corrections

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -41,7 +41,8 @@ class Rectangle:
 
         if self.__width == 0 or self.__height == 0:
             return ("")
-        string = ('#' * self.__width + '\n') * (self.__height)
+        string = ('#' * self.__width + '\n') * (self.__height - 1) + (
+            '#' * self.__width)
         return string
 
     def __repr__(self):
