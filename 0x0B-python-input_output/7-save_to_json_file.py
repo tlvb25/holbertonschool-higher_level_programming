b2dc96b6d1245e74fd0f6f36fe16302fed3447e3
T <test@mail.com>
2019-06-09 18:45:20 -0700

    code for task 7

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import json
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, mode='w', encoding='utf-8') as a_file:
-        json.dump(my_obj, a_file)
+
+def save_to_json_file(my_obj, filename=""):
+    
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
