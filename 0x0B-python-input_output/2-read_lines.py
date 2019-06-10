926a319f2a4c5c35fc836ede73fa33e9812bf48b
T <test@mail.com>
2019-06-09 18:03:34 -0700

    code for task 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
+    with open(filename, mode='r', encoding='utf-8') as a_file:
+        if (nb_lines <= 0):
+            print(a_file.read(), end="")
+        else:
+            for line in a_file:
+                print(line, end="")
+                nb_lines -= 1
+                
