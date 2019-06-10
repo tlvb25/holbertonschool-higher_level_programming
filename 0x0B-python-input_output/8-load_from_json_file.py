1a9a522bb2d66ad00ae9c78dc39a165f2a1de538
T <test@mail.com>
2019-06-09 18:54:08 -0700

    code changes

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -3,8 +3,6 @@ import json
 
 
 def load_from_json_file(filename=""):
-    '''
-        contains function that creates an Object from a “JSON file”
-    '''
+    
     with open(filename, mode='r', encoding='utf-8') as a_file:
         return json.load(a_file)
