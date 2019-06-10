3bae51cc43eb80788336805adfeb396c94048cdb
T <test@mail.com>
2019-06-09 18:13:33 -0700

    code for task 3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as a_file:
+        char_count = a_file.write(text)
+        return char_count
