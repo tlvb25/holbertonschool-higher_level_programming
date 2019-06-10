873079ff9cf369d8a00a08d5f2c9367fbae49808
T <test@mail.com>
2019-06-09 17:32:15 -0700

    code corrections

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,12 +3,13 @@
     contains method number_of_lines
 '''
 
+
 def number_of_lines(filename=""):
     '''
         returns num of lines in textfile
     '''
-    with open(filename, mode='r', encoding='utf-8') as a_file:
-        line_num = 0
-        for l in a_file:
-            line_num +=1
-        return (line_num)
+    lineNum = 0
+    with open(filename, mode='r', encoding='utf-8') as f:
+        for line in f:
+            lineNum += 1
+        return(lineNum)
