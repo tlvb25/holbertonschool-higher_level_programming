1f725a0ab8dd7e4caa0d020ee75afc59cc8a39f8
T <test@mail.com>
2019-06-09 17:26:30 -0700

    code for task 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
+    with open(filename, mode='r', encoding='utf-8') as f:
+        line_num = 0
+        for l in f:
+            line_num +=1
+        return line_num
