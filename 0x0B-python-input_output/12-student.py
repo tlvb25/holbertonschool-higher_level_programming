5733666d0ef769453343d0eeb005df9b39ba7455
T <test@mail.com>
2019-06-10 18:43:34 -0700

    code for task 12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+class Student:
+    
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not isinstance(attrs, list) or not all(
+            isinstance(strings, str) for strings in attrs):
+            return self.__dict__
+        dictionary = {}
+        for elements in attrs:
+            try:
+                dictionary[elements] = self.__dict__[elements]
+            except:
+                pass
+        return dictionary
