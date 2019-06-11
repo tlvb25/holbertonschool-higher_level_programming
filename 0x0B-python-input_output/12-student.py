49f8e209644958f6359ce989a959d63b3109a806
T <test@mail.com>
2019-06-10 19:00:15 -0700

    style changes

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -2,14 +2,13 @@ class Student:
 
 
     def __init__(self, first_name, last_name, age):
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         if not isinstance(attrs, list) or not all(
-            isinstance(strings, str) for strings in attrs):
+                isinstance(s, str) for s in attrs):
             return self.__dict__
         dictionary = {}
         for elements in attrs:
