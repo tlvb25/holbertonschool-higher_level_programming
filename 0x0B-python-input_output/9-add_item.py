691236e7dbf4751d73b01ffd67916b24b2ca61cd
T <test@mail.com>
2019-06-10 17:31:21 -0700

    code for task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+
+import sys
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+def add_item(args="", filename=""):
+    try:
+        all_args = load_from_json_file(filename)
+    except:
+        all_args = []
+
+    for arguments in args:
+        all_args.append(arguments)
+    save_to_json_file(all_args, filename)
+
+    if __name__ == "__main__":
+        args = sys.argv[1:]
+        filename = "add_item.json"
+        add_item(args, filename)
