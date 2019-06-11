e1aaf4860541c45d7e606ce14971210b2070759e
T <test@mail.com>
2019-06-10 18:04:15 -0700

    code changes

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 
-#!/usr/bin/python3
-"""Module 11-student"""
-
-
 class Student:
-    """Custom class Student"""
-
+    
     def __init__(self, first_name, last_name, age):
-        """Initialization function
-        Args:
-            first_name: first name of student
-            last_name: last name of student
-            age: age of student
-        """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Returns dictionary representation of a student instance"""
         return self.__dict__
