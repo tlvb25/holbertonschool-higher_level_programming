d141267c746633fd1fc8762dbbc4e359022f808f
T <test@mail.com>
2019-06-10 17:56:05 -0700

    code for task 11

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import json
+
+
+def class_to_json(obj):
+    '''
+        contains function that returns the dictionary
+        description with simple data structure
+    '''
+    return (getattr(obj, "__dict__"))
