a5bd99fb1c97df62a0703a09811600a983ccd1a2
T <test@mail.com>
2019-06-10 17:32:43 -0700

    code changes

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-
 import sys
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json = __import__("7-save_to_json_file").save_to_json_file
+load_from_json = __import__("8-load_from_json_file").load_from_json_file
+
 
 def add_item(args="", filename=""):
+    '''
+        contains cript that adds all arguments to a Python list
+        and then save them to a file
+    '''
     try:
-        all_args = load_from_json_file(filename)
+        all_args = load_from_json(filename)
     except:
         all_args = []
 
     for arguments in args:
         all_args.append(arguments)
-    save_to_json_file(all_args, filename)
+    save_to_json(all_args, filename)
 
-    if __name__ == "__main__":
-        args = sys.argv[1:]
-        filename = "add_item.json"
-        add_item(args, filename)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(args, filename)
