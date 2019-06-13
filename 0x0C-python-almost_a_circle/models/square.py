4146229f5f533cb7c53b43494a649b99e7134a91
T <test@mail.com>
2019-06-12 22:53:46 -0700

    style changes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(id=id, x=x, y=y, width=size, height=size)
 
     def __str__(self):
-        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y, self.width)
-    
+        return '[Square] ({}) {}/{} - {}'.format(
+                self.id, self.x, self.y, self.width)
+
     @property
     def size(self):
         return self.width
-    
+
     @size.setter
     def size(self, value):
         self.width = value
@@ -45,4 +47,4 @@ class Square(Rectangle):
             class_dict['x'] = self.x
             class_dict['y'] = self.y
             return class_dict
-            
+
