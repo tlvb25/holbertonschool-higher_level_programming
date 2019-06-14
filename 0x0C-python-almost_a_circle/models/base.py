68230f7a935fc3619016b8ca8d04f4760850a6cb
T <test@mail.com>
2019-06-14 14:47:53 -0700

    updated comment lengths

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,13 +9,13 @@ import os
 
 
 class Base:
-    """Base Class
+    """Base Class Used for other Classes
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """id  Attribute
+        """Method for id  Instantation Attribute
         """
         if id is not None:
             self.id = id
@@ -25,7 +25,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries=[]):
-        """Method for Serializing to Json
+        """Method for Serializing a list into json
         """
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
@@ -34,7 +34,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Method Save to File 
+        """Method to Save to a File after Serialized for json
         """
         list = [] if list_objs is None else [
                 x.to_dictionary() for x in list_objs]
@@ -43,7 +43,7 @@ class Base:
             a_file.write(cls.to_json_string(list))
 
     def from_json_string(json_string):
-        """Returns A json String
+        """Method that Returns A json String from File
         """
         if json_string is None or not json_string:
             return []
@@ -52,7 +52,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        """Creates an Updated Class Instance
+        """Method to Creates an Updated Class Instance
         """
         dummy = cls(8, 8, 8, 8)
         dummy.update(**dictionary)
@@ -60,7 +60,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Load from json file
+        """Method to load from a json File
         """
         filename = cls.__name__ + '.json'
         with open(filename, mode='r') as a_file:
