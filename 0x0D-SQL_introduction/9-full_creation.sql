21ccb608a1696fe9eb0e0d414ca9cd6716fec76d
T <test@mail.com>
2019-07-15 22:02:57 -0700

    script corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 -- script that creates a table second_table in the database and multiple rows
 CREATE TABLE IF NOT EXIST second_table (id INT,
 name VARCHAR(256), score INT);
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('1', "John", '10');
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('2', "Alex", '3');
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('3', "Bob", '14');
-INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('4', "George", '8');
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('1', "John", '10');
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('2', "Alex", '3');
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('3', "Bob", '14');
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('4', "George", '8');
