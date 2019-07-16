14f59899f44c6537b93eecfae7a46248bb65ed1f
T <test@mail.com>
2019-07-15 21:27:00 -0700

    code for task 6

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Full description
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
-SELECT * FROM first_table;
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- List all in table
+-- script that lists all rows of the table first_table from the database
+SELECT * FROM first_table;
