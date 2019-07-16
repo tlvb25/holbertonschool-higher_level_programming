b6e68a74ad59c999c0d721d919b7ecd18f0f8ca4
T <test@mail.com>
2019-07-15 20:54:46 -0700

    code for task 4

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- List Tables
+-- script that lists all the tables of a database in your MySQL server.
+ SHOW tables;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- First Table
+-- script that creates a table called first_table in the current database
+CREATE TABLE first_table (id INT, 
+name VARCHAR(256),
+food VARCHAR(30);
