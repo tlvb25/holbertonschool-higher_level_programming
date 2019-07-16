93ad8628e8372276eac384329535f0d0e58d8e8c
T <test@mail.com>
2019-07-15 23:42:07 -0700

    scripting for advanced task

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,6 @@
 -- Temperatures #0
 -- Import in hbtn_0c_0 database this table dump
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp 
+FROM temperatures 
+GROUP BY city 
+ORDER BY avg_temp DESC;
