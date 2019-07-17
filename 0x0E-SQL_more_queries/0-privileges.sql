3018d621db55c55be57969ef6180f8c80c395d41
T <test@mail.com>
2019-07-16 22:17:34 -0700

    CORRECTION

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- My privileges
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
