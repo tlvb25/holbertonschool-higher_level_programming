48ea5f1b8cdb91a1f46d43c29f5e8ce4ee54fe6f
T <test@mail.com>
2019-07-16 23:41:37 -0700

    script corrections

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- Cities of California
 SELECT id, name FROM cities 
-WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY id;
+WHERE state_id=(SELECT id FROM states WHERE name='California') 
+ORDER BY id;
