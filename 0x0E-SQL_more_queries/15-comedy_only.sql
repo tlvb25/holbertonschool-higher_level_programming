8210b95881695bc766281223ce4b2e97e3f1a215
T <test@mail.com>
2019-07-17 23:43:37 -0700

    correction

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 -- ONLY COMEDY
-SELECT tv_genres.name
-FROM tv_show
+SELECT tv_shows.title
+FROM tv_shows
 INNER JOIN tv_genres
-ON tv_show.id=tv_genres.id
+ON tv_shows.id=tv_genres.id
 INNER JOIN tv_show_genres
 ON tv_show_genres.show_id=tv_genres.id
 WHERE tv_genres.name='Comedy'
