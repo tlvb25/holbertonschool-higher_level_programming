267974a5255ce4fb676c2239fbbe19322436080b
T <test@mail.com>
2019-07-17 23:45:24 -0700

    correction

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,6 +4,6 @@ FROM tv_shows
 INNER JOIN tv_genres
 ON tv_shows.id=tv_genres.id
 INNER JOIN tv_show_genres
-ON tv_show_genres.show_id=tv_genres.id
+ON tv_show_genres.genre_id=tv_genres.id
 WHERE tv_genres.name='Comedy'
 ORDER BY tv_shows.title ASC;
