fcf48e265879c740eddf4fe1da7e76ebd9957d8e
T <test@mail.com>
2019-07-17 23:59:39 -0700

    CORRECTION

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
 RIGHT JOIN tv_shows
-ON tv_show_genres.show_id=tv.shows_id
+ON tv_show_genres.show_id=tv_shows.id
 LEFT JOIN tv_genres
-ON tv_genres.id=tv_show_genres.genres_id
-ORDER BY tv_shows.title, tv_genres.name ASC;
+ON tv_show_genres.genre_id=tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
