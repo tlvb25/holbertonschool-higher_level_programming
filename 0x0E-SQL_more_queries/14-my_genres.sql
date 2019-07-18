f45e56d365d41aa9b30f11b8d5ab7b94773db2b6
T <test@mail.com>
2019-07-17 23:29:23 -0700

    correction

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
 -- MY GENRES
-SELECT tv_genres.name AS 'name'
-FROM tv_genres
-INNER JOIN tv_shows, tv_show_genres
-ON tv_genres.id=tv_show_genres.show_id=tv_shows.id
-WHERE tv_shows.id = "Dexter"
+SELECT tv_genres.name
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_show_genres.show_id=tv_shows.id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id=tv_genres.id
+WHERE tv_shows.title='Dexter'
 ORDER BY tv_genres.name ASC;
