2a32164f16f820c7279f45b43b33317d1717f2c1
T <test@mail.com>
2019-07-17 23:19:22 -0700

    script for task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- MY GENRES
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_shows, tv_show_genres
+ON tv_genres.id=tv_show_genres.show_id=tv_shows.id
+WHERE tv_shows.id='Dexter'
+ORDER BY tv_genres.name ASC;
