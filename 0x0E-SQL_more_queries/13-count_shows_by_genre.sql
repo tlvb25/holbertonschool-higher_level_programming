30dbdf79df4964a1997a8969b2c4c5d560d9aa3f
T <test@mail.com>
2019-07-17 21:15:07 -0700

    CORRECTION

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Number of shows by genre
-SELECT tv_genres.name as genre, COUNT(*) as 'number_of_shows'
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id=tv_genres.id
