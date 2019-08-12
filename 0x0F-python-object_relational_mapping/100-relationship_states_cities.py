956e14683a8ae7c286f009245a2cbeb8113e004a
T <test@mail.com>
2019-08-12 13:16:09 -0700

    code chg

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Create California"""
-import argv
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import (create_engine)
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
