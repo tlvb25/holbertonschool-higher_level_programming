2d1cdd692a2a3c3f1928a145c05edc654c3c01cd
T <test@mail.com>
2019-08-12 00:10:50 -0700

    code chg

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """script changes the name of a State"""
 from sys import argv
-from sqlalchemy import create_engine
 from model_state import Base, State
-from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
+    
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
 
-    state = session.query(State).filter(State.id == 2).all()
+    states = session.query(State).filter(State.id == 2).all()
 
-    if state:
-        state[0].name = "New Mexico"
+    if states:
+        states[0].name = "New Mexico"
     session.commit()
     session.close()
