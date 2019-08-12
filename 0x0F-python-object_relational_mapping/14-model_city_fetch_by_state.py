f1c71619af6ae91fe1990b99c688c2a5239114e2
T <test@mail.com>
2019-08-12 00:31:20 -0700

    code chg

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
-import sys
+"""script lists all states from the database"""
+from sys import argv
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from model_city import City
@@ -10,10 +10,15 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
+
     session = Session(engine)
-    for city, state in session.query(City, State)\
-                              .filter(State.id == City.state_id)\
-                              .order_by(City.id).all():
+
+    query = session.query(City, State)\
+        .filter(State.id == City.state_id)\
+        .order_by(City.id).all()
+
+    for city, state in query:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
