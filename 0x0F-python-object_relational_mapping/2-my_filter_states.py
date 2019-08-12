9c625ad4acd7b634aa3c0d26e45d83ecddbd9907
T <test@mail.com>
2019-08-11 22:29:01 -0700

    code changes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-
+    
     # storing entire database connection into variable 'db'
     db = MySQLdb.connect(host="localhost",
                          port=3306,
@@ -19,7 +19,8 @@ if __name__ == "__main__":
 
     # # HERE I have to know SQL to grab all states in my database
     cur.execute("""SELECT * FROM states
-    WHERE name LIKE BINARY 'N%' ORDER BY id""")
+    WHERE name LIKE BINARY '{}' ORDER BY id"""
+               .format(argv[4]))
 
     # all rows in the states table
     query_rows = cur.fetchall()
