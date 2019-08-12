9edf202f0ecda677b1366314c58132631a3652e8
T <test@mail.com>
2019-08-12 00:27:47 -0700

    code chg

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""Adds a brand new State to the system!"""
-
-from sys import argv
-from model_state import Base, State
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 from model_city import City
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    rows = session.query(City, State).\
-        filter(City.state_id == State.id).all()
-    for city, state in rows:
+    session = Session(engine)
+    for city, state in session.query(City, State)\
+                              .filter(State.id == City.state_id)\
+                              .order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.commit()
     session.close()
