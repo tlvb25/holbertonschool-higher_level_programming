4fe7c39dacc3a54c56813059539626162bd7ffbe
T <test@mail.com>
2019-08-11 23:50:33 -0700

    code corr

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,13 +8,17 @@ from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     session = Session(engine)
-    query = \
-        session.query(State).filter(State.name == sys.argv[4]).first()
+
+    query = session.query(State)\
+        .filter(State.name == sys.argv[4]).first()
+
     if query:
         print(query.id)
     else:
