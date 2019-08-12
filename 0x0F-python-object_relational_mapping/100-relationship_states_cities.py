e9527b06ed90d7394fb49539ee796c0b33b850d3
T <test@mail.com>
2019-08-12 00:47:16 -0700

    code change

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""create state california and city"""
-from sys import argv
+"""Improve the files model_city.py and model_state.py, and save
+them as relationship_city.py and relationship_state.py"""
+import sys
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from relationship_state import State, Base
-from relationship_city import City
+from relationship_state import State
+from relationship_city import City, Base
 
 
 if __name__ == "__main__":
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-         
     Base.metadata.create_all(engine)
-
     session = Session(engine)
-
-    state = State(name="California")
-    session.add(state)
-    session.commit()
-    city = City(name="San Francisco", state_id=new_state.id)
-    session.add(city)
+    s1 = State(name='California')
+    c1 = City(name='San Francisco')
+    s1.cities.append(c1)
+    session.add_all([c1, s1])
     session.commit()
     session.close()
