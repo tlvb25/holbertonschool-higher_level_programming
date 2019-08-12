ad1c68c92f089013040150d69dad0ad9134732fc
T <test@mail.com>
2019-08-11 22:52:50 -0700

    code corr

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,15 +10,16 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          charset="utf-8",
-                         user=argv[1],  
-                         passwd=argv[2], # using argv[] variables directly
+                         user=argv[1],
+                         passwd=argv[2],  # using argv[] variables directly
                          db=argv[3])     # using argv[] variables directly
 
     # must use databse cursor obj in order to execute queries
     cur = db.cursor()
 
     # # HERE I have to know SQL to grab all states in my database
-    cur.execute("""SELECT * FROM states WHERE WHERE name = %s ORDER BY id ASC""".format(argv[4]))
+    cur.execute("""SELECT * FROM states WHERE WHERE name=%s ORDER BY id ASC"""
+        .format(argv[4]))
 
     # all rows in the states table
     query_rows = cur.fetchall()
