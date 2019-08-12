775297384472cee78eecf58c38236d3ca1819412
T <test@mail.com>
2019-08-11 23:49:58 -0700

    code corr

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""State matching argv[4]"""
-
-from sys import argv
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""script lists State objects that contain
+ the letter 'a' """
+import sys
+from sqlalchemy import create_engine
 from model_state import Base, State
+from sqlalchemy.orm import Session
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = session(engine)
-    query = session.query(State)\
-        .filter(State.name == argv[4]).first()
-
+    session = Session(engine)
+    query = \
+        session.query(State).filter(State.name == sys.argv[4]).first()
     if query:
         print(query.id)
     else:
-        print("Not found")
+        print('Not found')
     session.close()
