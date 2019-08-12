b547224a0fad8c87e518188f23682efd4ee26bf1
T <test@mail.com>
2019-08-12 00:21:47 -0700

    code change

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
+"""script that lists all states from database"""
 from sys import argv
-from model_city import City
-from model_state import Base, State
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+from model_city import City
+from model_state import Base, State
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
+
     session = Session(engine)
 
-    query = session.query(City, State).filter(State.id == City.state_id)\
+    query = session.query(City, State)\
+            .filter(State.id == City.state_id)\
             .order_by(City.id).all()
 
     for city, state in query:
