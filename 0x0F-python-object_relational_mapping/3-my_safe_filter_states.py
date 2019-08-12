86ed89aae226b611613d53c1b1a9f6a20bedd1dd
T <test@mail.com>
2019-08-11 23:03:58 -0700

    code curr

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""script lists all states name beg "N" from db hbtn_0e_0_usa"""
+"""Lists all states starting with passed arg, prevents injection"""
+
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-
-    # storing entire database connection into variable 'db'
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         charset="utf-8",
-                         user=argv[1],
-                         passwd=argv[2],  # using argv[] variables directly
-                         db=argv[3])     # using argv[] variables directly
-
-    # must use databse cursor obj in order to execute queries
+    
+    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    
     cur = db.cursor()
 
-    # # HERE I have to know SQL to grab all states in my database
     cur.execute(
         "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
         (argv[4], ))
-
-    # all rows in the states table
-    query = cur.fetchall()
-    for row in query:
+    
+    query_rows = cur.fetchall()
+    
+    for row in query_rows:
         print(row)
     cur.close()
     db.close()
