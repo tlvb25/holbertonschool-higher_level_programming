4f7959c8988edfd2be31145ee3f7b69dcf89ca85
T <test@mail.com>
2019-08-12 09:48:54 -0700

    code chg

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur.execute("""SELECT * FROM states
     WHERE name LIKE BINARY 'N%' ORDER BY id""")
     query = cur.fetchall()
-    for i in query:
-        print(i)
+    for row in query:
+        print(row)
     cur.close()
     conn.close()
