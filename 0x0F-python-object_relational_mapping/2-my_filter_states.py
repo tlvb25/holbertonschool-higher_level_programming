4927ade8dfd2d44b47661958fb41cf362c23e62e
T <test@mail.com>
2019-08-11 22:32:47 -0700

    code changes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""script lists all states name beg "N" from db hbtn_0e_0_usa"""
-import MySQLdb
+"""script takes an argument and displays all rows in states table
+ of database hbtn_0e_0_usa where name matches the argument"""
 from sys import argv
+import MySQLdb
 
 
-if __name__ == "__main__":
-    
-    # storing entire database connection into variable 'db'
+if __name__ == '__main__':
+    user, password, database, state = argv[1], argv[2], argv[3], argv[4]
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         charset="utf-8",
-                         user=argv[1],   # using argv[] variables directly
-                         passwd=argv[2], # using argv[] variables directly
-                         db=argv[3])     # using argv[] variables directly
-
-    # must use databse cursor obj in order to execute queries
+                         user=user, passwd=password, db=database)
     cur = db.cursor()
-
-    # # HERE I have to know SQL to grab all states in my database
     cur.execute("""SELECT * FROM states
     WHERE name LIKE BINARY '{}' ORDER BY id"""
-               .format(argv[4]))
-
-    # all rows in the states table
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+               .format(state))
+    query = cur.fetchall()
+    for i in query:
+        print(i)
     cur.close()
     db.close()
     
