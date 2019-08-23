859b46bbdfc172644261f162ce9ea5c3cd6c96ee
T <test@mail.com>
2019-08-22 17:58:39 -0700

    code for task 6

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends request to URL passed as argument, displays status code
 curl -so /dev/null -w "%{http_code}" "$1"
+https://github.com/tlvb25/holbertonschool-higher_level_programming.git
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# function that finds a peak in a list of unsorted integers
+
+def find_peak(list_of_integers):
+    l = len(list_of_integers)
+    if l == 0:
+        return None
+    if l == 1:
+        return l[0]
+    if l == 2:
+        return max(list_of_integers)
+
+    mid = list_of_integers//2
+
+    peak = list_of_integers[mid]
+    left = list_of_integers[mid - 1]
+    right = list_of_integers[mid + 1]
+
+    if peak > left and peak > right:
+        return peak
+    elif peak < left:
+        return list_of_integers[:mid]
+    else:
+        return list_of_integers[mid:]
