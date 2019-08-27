13b921650a060f2eb5e6e4e914ff1c487aff72a6
T <test@mail.com>
2019-08-26 22:49:13 -0700

    code corr

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Python script that takes your Github credentials (username and password)
-and uses the Github API to display your id"""
+""" My Github! """
 from sys import argv
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-    response = requests.get('https://api.github.com/user',
-            auth=(argv[1], argv[2]))
-    if response.status_code == 200:
-        j = response.json()
+    r = requests.get('https://api.github.com/user',
+                     auth=(argv[1], argv[2]))
+    if r.status_code == 200:
+        j = r.json()
         print(j.get('id'))
     else:
         print('None')
