cd529c5f853c633842bda454f948245e59a5d94d
T <test@mail.com>
2019-08-26 23:21:32 -0700

    code corr

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get('https://api.github.com/user',
-                auth=(argv[1], argv[2]))
+        auth=(argv[1], argv[2]))
     if response.status_code == 200:
         j = response.json()
         print(j.get('id'))
