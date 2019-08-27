1a1af02e57ee92c879d1cf454ddcd7611b77a23d
T <test@mail.com>
2019-08-27 00:21:19 -0700

    code corr

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Star Wars API #2 """
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get('https://swapi.co/api/people/?search={}'.format(argv[1]))
+    j = response.json()
+    l = j.get('results')
+    print('Number of results:', j.get('count'))
+    while j.get('next') is not None:
+        response = requests.get(j.get('next'))
+        j = response.json()
+        l += j.get('results')
+    for i in l:
+        print(i.get('name'))
+        for a in i.get('films'):
+            film = requests.get(a)
+            print('\t{}'.format(film.json().get('title')))
