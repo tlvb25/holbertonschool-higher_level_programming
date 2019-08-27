de447e791d12510f20fd1067c2f64c101e754e10
T <test@mail.com>
2019-08-26 19:14:33 -0700

    code task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import urllib.request
+"""urlib.request usage"""
 
 if __name__ == "__main__":
     with urllib.request.urlopen('http://python.org/') as response:
         html = response.read()
-        print(response)
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('t- content: {}'.format(html))
+        print('t- utf8 content'.format(html.decode(utf-8)))
