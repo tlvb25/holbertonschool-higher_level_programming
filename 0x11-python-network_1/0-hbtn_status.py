3d8a7a8d33518c870c00e731684a8e9a5807c802
T <test@mail.com>
2019-08-26 19:06:00 -0700

    test code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('http://python.org/') as response:
+        html = response.read()
+        print(response)
