ab6c051c35fae338b8e118a1bccf0454c0c4fb8b
T <test@mail.com>
2019-08-26 19:17:48 -0700

    code corr

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """urlib.request usage"""
 
-import urllib.request
-
 if __name__ == "__main__":
-    with urllib.request.urlopen('http://python.org/') as response:
+
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
+        print("Body html:")
         print('\t- type: {}'.format(type(html)))
-        print('t- content: {}'.format(html))
-        print('t- utf8 content'.format(html.decode(utf-8)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
