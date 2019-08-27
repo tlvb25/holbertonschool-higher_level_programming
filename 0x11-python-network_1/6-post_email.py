15f25d215351323c68b05ce2a847580a10804e5c
T <test@mail.com>
2019-08-26 23:06:18 -0700

    code corr

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    response = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-    print(response.text)
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
