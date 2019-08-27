36a373432a62ffd384b309fd0d7c3c94dbafb36f
T <test@mail.com>
2019-08-26 23:34:48 -0700

    code for task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Time for an interview!  """
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    response = requests.get("https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1]))
+    if response.status_code != 200:
+        print("None")
+    else:
+        data = response.json()
+        for t in data[0:10]:
+            print("{}: {}".format(t.get('sha'),
+                                  t.get('commit').get('author').get('name')))
