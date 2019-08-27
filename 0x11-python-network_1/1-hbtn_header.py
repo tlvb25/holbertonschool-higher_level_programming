cfde65909d14c0c7a83edf17480d3f7ff754b8af
T <test@mail.com>
2019-08-26 22:36:29 -0700

    code for task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,5 @@ if __name__ == "__main__":
     import urllib.request
     import sys
 
-
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(dict(response.info()).get('X-Request-Id'))
