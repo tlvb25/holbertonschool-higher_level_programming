36676165304f2e0ab8b50449c7196fd0e780afa0
T <test@mail.com>
2019-08-26 23:20:13 -0700

    code chg

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,10 +5,10 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/user',
+    response = requests.get('https://api.github.com/user',
                      auth=(argv[1], argv[2]))
-    if r.status_code == 200:
-        j = r.json()
+    if response.status_code == 200:
+        j = response.json()
         print(j.get('id'))
     else:
         print('None')
