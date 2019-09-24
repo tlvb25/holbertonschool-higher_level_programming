2d6b34354e537a026d6fbde1149f9dd1cbaf5b87
T <test@mail.com>
2019-09-23 23:47:23 -0700

    style corr

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const reverseList = [];
-    const len = list.length - 1;
+  const reverseList = [];
+  const len = list.length - 1;
   
-    for (let i = len; i >= 0; i--) {
-      reverseList.push(list[i]);
-    }
-    return reverseList;
-  };
-  
+  for (let i = len; i >= 0; i--) {
+    everseList.push(list[i]);
+  }
+  return reverseList;
+};
