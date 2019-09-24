bce1ead40d75289b22410fbc091f637489f0b877
T <test@mail.com>
2019-09-23 23:50:00 -0700

    style corr

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,9 +3,9 @@
 exports.esrever = function (list) {
   const reverseList = [];
   const len = list.length - 1;
-  
+
   for (let i = len; i >= 0; i--) {
-    everseList.push(list[i]);
+    reverseList.push(list[i]);
   }
   return reverseList;
 };
