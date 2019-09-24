3c71abb32317f5f8fbb5a406b8a6af4707713461
T <test@mail.com>
2019-09-24 00:09:14 -0700

    style corr

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+
 const dict = require('./101-data').dict;
+
+const input = Object.input(dict);
 const newDict = {};
-for (const key in dict) {
-  const value = dict[key];
-  if (newDict[value] === undefined) {
-    newDict[value] = [key];
-  } else {
+for (const [key, value] of input) {
+  if (value in newDict) {
     newDict[value].push(key);
+  } else {
+    newDict[value] = [key];
   }
 }
+
 console.log(newDict);
