4640c14ccfd831bf2578f2081c7aabd8f45dc4a2
T <test@mail.com>
2019-09-24 21:02:50 -0700

    corr

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,5 +3,9 @@ const x = parseInt(process.argv[2]);
 if (NaN(x)) {
   console.log('Missing number of occurrences')
 } else {
-  console.log('C is fun\n'.repeat(x))
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
