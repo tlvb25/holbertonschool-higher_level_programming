e82fce3c40fa22df797a574015d4a7d94e868381
T <test@mail.com>
2019-09-24 20:55:57 -0700

    corr

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = [“C is fun”, “Python is cool”, “Javascript is amazing”];
-for (let i = 0, i < arr.length, i++) {
+const arr = ['C is fun”, “Python is cool”, “Javascript is amazing'];
+for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
